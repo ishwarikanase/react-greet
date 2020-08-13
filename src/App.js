8a971fb42bcf22bff898451b91d42887af490331
Ishwari Kanase <ishwari.kanase@gmail.com>
2020-08-13 19:13:35 +0530

    using jsx

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/greet';
 import Welcome from './components/welcome';
+import Hello from './components/hello';
+
 function App() {
   return (
     <div className="App">
       <Greet />
       <Welcome />
+      <Hello/>
     </div>
   );
 }
---
+++ src/components/hello.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Hello = () => {
+    return React.createElement('div',{id:'helo'},React.createElement('h1',null,'hello ishwari'))
+}
+export default Hello;
