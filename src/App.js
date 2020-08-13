03b9df98c63d3b23bc3bfb3303106d888bab75a0
Ishwari Kanase <ishwari.kanase@gmail.com>
2020-08-13 12:08:44 +0530

    functional and class component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/greet';
+import Welcome from './components/welcome';
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/welcome.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component{
+    render(){
+        return <h1>Welcome ishwari</h1>
+    }
+}
+export default Welcome;
